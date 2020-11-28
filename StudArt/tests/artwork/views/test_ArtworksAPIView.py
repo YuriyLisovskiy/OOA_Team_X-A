5f6c529e6667213a0f06024c6fd652243e8160d5
oshipka <olenkashipka@gmail.com>
2020-11-28 12:33:13 +0200

    tested ArtworksAPIView

--- StudArt/tests/artwork/views/test_ArtworksAPIView.py
+++ StudArt/tests/artwork/views/test_ArtworksAPIView.py
@@ -9,9 +9,58 @@ class ArtworksAPITestCase(APIFactoryTestCase):
 		super(ArtworksAPITestCase, self).setUp()
 		self.view = ArtworksAPIView.as_view()
 
-	def test_getArtworksTwoColumnsOnePage(self):
+	def test_getArtworksTwoColumnsFirstPage(self):
 		request = self.request_factory.get('/api/v1/artworks', {'columns': 2, 'page': 1})
 		response = self.view(request)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(response.data['count'], 3)
+		self.assertEqual(response.data['count'], 12)
 		self.assertEqual(len(response.data['results']), 2)
+
+	def test_getArtworksFourColumnsFirstPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 4, 'page': 1})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 12)
+		self.assertEqual(len(response.data['results']), 4)
+
+	def test_getArtworksFourColumnsSecondPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 4, 'page': 2})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 12)
+		self.assertEqual(len(response.data['results']), 4)
+
+	def test_getArtworksOutOfBoundsPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 3, 'page': 5})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_getArtworksFilterByTagFirstPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 3, 'page': 1, 'tag': 'tag1'})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 5)
+		self.assertEqual(len(response.data['results']), 3)
+
+	def test_getArtworksFilterByTagSecondPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 3, 'page': 2, 'tag': 'tag1'})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 5)
+		self.assertEqual(len(response.data['results']), 2)
+
+	def test_getArtworksFilterByAuthorFirstPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 3, 'page': 1, 'author': 'User'})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 6)
+		self.assertEqual(len(response.data['results']), 3)
+
+	def test_getArtworksFilterByAuthorSecondPage(self):
+		request = self.request_factory.get('/api/v1/artworks', {'columns': 3, 'page': 2, 'author': 'User'})
+		response = self.view(request)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['count'], 6)
+		self.assertEqual(len(response.data['results']), 3)
+
+# todo: test filter_by_subscriptions
--- StudArt/tests/common.py
+++ StudArt/tests/common.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase, APIRequestFactory
 
-from artwork.models import ArtworkModel
+from artwork.models import ArtworkModel, TagModel
 from core.models import UserModel
 
 
@@ -13,7 +13,33 @@ class APIFactoryTestCase(APITestCase):
 	def setUpTestData(cls):
 		# Set up non-modified objects used by all test methods
 		UserModel.objects.create(username='User', email='mail@mail.com')
-		ArtworkModel.objects.create(description='Some description 1', author=UserModel.objects.first())
-		ArtworkModel.objects.create(description='Some description 2', author=UserModel.objects.first())
-		ArtworkModel.objects.create(description='Some description 3', author=UserModel.objects.first())
-
+		UserModel.objects.create(username='User2', email='mail2@mail.com')
+		TagModel.objects.create(text='tag1')
+		TagModel.objects.create(text='tag2')
+		TagModel.objects.create(text='tag3')
+		TagModel.objects.create(text='tag4')
+		TagModel.objects.create(text='tag5')
+		ArtworkModel.objects.create(description='Some description 1', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 2', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 3', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 4', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 5', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 6', author=UserModel.objects.get(username='User'))
+		ArtworkModel.objects.create(description='Some description 7', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.create(description='Some description 8', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.create(description='Some description 9', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.create(description='Some description 10', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.create(description='Some description 11', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.create(description='Some description 12', author=UserModel.objects.get(username='User2'))
+		ArtworkModel.objects.get(description='Some description 1').tags.set([TagModel.objects.get(text='tag1'), TagModel.objects.get(text='tag3')])
+		ArtworkModel.objects.get(description='Some description 2').tags.set([TagModel.objects.get(text='tag2'), TagModel.objects.get(text='tag4')])
+		ArtworkModel.objects.get(description='Some description 3').tags.set([TagModel.objects.get(text='tag3'), TagModel.objects.get(text='tag5')])
+		ArtworkModel.objects.get(description='Some description 4').tags.set([TagModel.objects.get(text='tag4'), TagModel.objects.get(text='tag1')])
+		ArtworkModel.objects.get(description='Some description 5').tags.set([TagModel.objects.get(text='tag5'), TagModel.objects.get(text='tag2')])
+		ArtworkModel.objects.get(description='Some description 6').tags.set([TagModel.objects.get(text='tag1'), TagModel.objects.get(text='tag3')])
+		ArtworkModel.objects.get(description='Some description 7').tags.set([TagModel.objects.get(text='tag2'), TagModel.objects.get(text='tag4')])
+		ArtworkModel.objects.get(description='Some description 8').tags.set([TagModel.objects.get(text='tag3'), TagModel.objects.get(text='tag5')])
+		ArtworkModel.objects.get(description='Some description 9').tags.set([TagModel.objects.get(text='tag4'), TagModel.objects.get(text='tag1')])
+		ArtworkModel.objects.get(description='Some description 10').tags.set([TagModel.objects.get(text='tag5'), TagModel.objects.get(text='tag2')])
+		ArtworkModel.objects.get(description='Some description 11').tags.set([TagModel.objects.get(text='tag1'), TagModel.objects.get(text='tag3')])
+		ArtworkModel.objects.get(description='Some description 12').tags.set([TagModel.objects.get(text='tag2'), TagModel.objects.get(text='tag4')])
