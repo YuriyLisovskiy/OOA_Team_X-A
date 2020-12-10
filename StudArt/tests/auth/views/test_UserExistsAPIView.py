166ebbc043a25d269ae3974bc804e0d2efa1e875
oshipka <olenkashipka@gmail.com>
2020-12-10 17:50:15 +0200

    created user tests

---
+++ StudArt/tests/auth/views/test_UserExistsAPIView.py
@@ -0,0 +1,17 @@
+import json
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import force_authenticate
+from rest_framework_simplejwt.state import User
+
+from auth.views import UserExistsAPIView
+from tests.common import APIFactoryTestCase
+
+
+class RegisterUserAPITestCase(APIFactoryTestCase):
+	def setUp(self) -> None:
+		super(RegisterUserAPITestCase, self).setUp()
+		self.view = UserExistsAPIView.as_view()
+		self.user_admin = User.objects.get(username='admin')
+
